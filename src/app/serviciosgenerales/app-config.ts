7863e7dde0b489254a7d52071be8d99ceff7c912
mramosobinte <mramos@obinte.com>
2019-01-04 08:32:01 -0500

    eee

--- src/app/serviciosgenerales/app-config.ts
+++ src/app/serviciosgenerales/app-config.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class AppConfig {
 
     //public baseApiPath: string = "https://tws.todocompu.com.ec:1443/Obinte/";
-    public baseApiPath: string = "http://192.168.0.141:8080/ShrimpSoftServer/";
+    public baseApiPath: string = "http://localhost:8080/ShrimpSoftServer/";
     
     constructor() {
 
